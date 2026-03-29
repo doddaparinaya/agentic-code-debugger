cb44c0c4a3dfe480b3183f6461d505e91b290f15
Dodda Parinaya <109713445+doddaparinaya@users.noreply.github.com>
2026-03-29 18:34:33 +0530

    Add files via upload

---
+++ Agents/LogicReasoner.py
@@ -0,0 +1,15 @@
+from core.llamaInterface import query_llama
+
+class LogicReasoner:
+    def __init__(self, model='llama3.2'):
+        self.model = model
+    
+    def reason(self, code: str) -> str:
+        prompt = f"""You are a Python logic explainer. Read the following function and explain 
+        in simple terms what it is supposed to do:
+        
+        {code}
+        Respond with only:
+        - intended Logic:
+        """
+        return query_llama(prompt, model=self.model)
